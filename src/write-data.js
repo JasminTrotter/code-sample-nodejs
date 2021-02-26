6640dab4207e9757d71780d210f835dc34bdca32
Jasmin Trotter <34755222+JasminTrotter@users.noreply.github.com>
2021-02-26 11:27:38 -0800

    feat: read-data & write-data handlers

--- src/write-data.js
+++ src/write-data.js
@@ -1,11 +1,7 @@
-const AWS = require('aws-sdk');
+const dynamodb = require('./dynamo-client')
 
-const dynamodb = new AWS.DynamoDB.DocumentClient({
-  apiVersion: '2012-08-10',
-  endpoint: new AWS.Endpoint('http://localhost:8000'),
-  region: 'us-west-2',
-  // what could you do to improve performance?
-});
+// what could you do to improve performance?
+const dynamoClient = dynamodb.getDynamo()
 
 const tableName = 'SchoolStudents';
 
@@ -21,7 +17,48 @@ const tableName = 'SchoolStudents';
  * @param {string} event.studentGrade
  */
 exports.handler = (event) => {
-  // TODO validate that all expected attributes are present (assume they are all required)
-  // TODO use the AWS.DynamoDB.DocumentClient to save the 'SchoolStudent' record
+  // T̶O̶D̶O̶ validate that all expected attributes are present (assume they are all required)
+  // T̶O̶D̶O̶ use the AWS.DynamoDB.DocumentClient to save the 'SchoolStudent' record
   // The 'SchoolStudents' table key is composed of schoolId (partition key) and studentId (range key).
+
+  return new Promise((resolve, reject) => {
+    const expectedAttributes = [
+      'schoolId',
+      'schoolName',
+      'studentId',
+      'studentFirstName',
+      'studentLastName',
+      'studentGrade'
+    ];
+    const missingAttributes = expectedAttributes.reduce((missing, attribute) => {
+      if (!event[attribute]) {
+        missing.push(attribute)
+      }
+      return missing
+    }, []);
+
+    if (missingAttributes.length) {
+      reject(new Error(`missing attributes: ${missingAttributes}`))
+    }
+
+    const params = {
+      RequestItems: {
+        'SchoolStudents': [
+          {
+            PutRequest: {
+              Item: { ...event }
+            }
+          }
+        ]
+      }
+    };
+
+    dynamoClient.batchWrite(params, (err, data) => {
+      if (err) {
+        reject(new Error(err))
+      } else {
+        resolve(data)
+      }
+    });
+  });
 };
